44f40b66ccab3f5bb33e664ea9c6456547b72ce1
TrentBrunson <66187531+TrentBrunson@users.noreply.github.com>
2020-08-05 19:23:52 -0500

    budget parsing passed; dropping mostly empty columns next

--- challenge2.py
+++ challenge2.py
@@ -125,6 +125,46 @@ wiki_movies_df.drop_duplicates(subset='imdb_id', inplace=True)
 wiki_columns_to_keep = [column for column in wiki_movies_df.columns if wiki_movies_df[column].isnull().sum() < len(wiki_movies_df) * 0.9]
 wiki_movies_df = wiki_movies_df[wiki_columns_to_keep]
 
+# matching strings in box office
+# have two string types "$ddd.d million or billon" & "$ddd,ddd,ddd"
+form_one = r'\$\s*\d+\.?\d*\s*[mb]illi?on'
+form_two = r'\$\s*\d{1,3}(?:[,\.]\d{3})+(?!\s[mb]illion)'
 
+
+def parse_dollars(s): # setting function to
+    # if s is not a string, return NaN
+    if type(s) != str:
+        return np.nan
+
+    # if input is of the form $###.# million
+    if re.match(r'\$\s*\d+\.?\d*\s*milli?on', s, flags=re.IGNORECASE):
+        # remove dollar sign and " million"
+        s = re.sub(r'\$|\s|[a-zA-Z]', '', s)
+        # convert to float and multiply by a million
+        value = float(s) * 10**6
+        # return value
+        return value
+
+    # if input is of the form $###.# billion
+    elif re.match(r'\$\s*\d+\.?\d*\s*billi?on', s, flags=re.IGNORECASE):
+        # remove dollar sign and " billion"
+        s = re.sub(r'\$|\s|[a-zA-Z]','', s)
+        # convert to float and multiply by a billion
+        value = float(s) * 10**9
+        # return value
+        return value
+
+    # if input is of the form $###,###,###
+    elif re.match(r'\$\s*\d{1,3}(?:[,\.]\d{3})+(?!\s[mb]illion)', s, flags=re.IGNORECASE):
+        # remove dollar sign and commas
+        s = re.sub(r'\$|,','', s)
+        # convert to float
+        value = float(s)
+
+        # return value
+        return value
+    # otherwise, return NaN
+    else:
+        return np.nan
 
 
